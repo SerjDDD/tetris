95336f647d5746ceb74a547d68c8b6ba9f9c1b08
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-27 13:26:40 +0000

    Deploy SerjDDD/tetris to github.com/SerjDDD/tetris.git:gh-pages

---
+++ precache-manifest.4a468370b9edd7309cffe8aaac2929c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c33649fc0ed8cf9d6a50d644837468",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "c93559e731b95dfb84aa",
+    "url": "/tetris/static/css/main.52805be3.chunk.css"
+  },
+  {
+    "revision": "92c027925ad7e7cb38f8",
+    "url": "/tetris/static/js/2.accbef2b.chunk.js"
+  },
+  {
+    "revision": "c93559e731b95dfb84aa",
+    "url": "/tetris/static/js/main.79510a22.chunk.js"
+  },
+  {
+    "revision": "e9e58439d3b8c64431c2",
+    "url": "/tetris/static/js/runtime~main.61707ba2.js"
+  },
+  {
+    "revision": "4fbb4aacfba8b385be8a125cd22bffd5",
+    "url": "/tetris/static/media/tetris-webfont.4fbb4aac.woff"
+  },
+  {
+    "revision": "d7200a33cc0fd5c946407ed573a3a8ee",
+    "url": "/tetris/static/media/tetris-webfont.d7200a33.woff2"
+  }
+]);
